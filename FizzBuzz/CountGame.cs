9b080375678606806c7f823cffa8915f084e8b99
Jie Li <JLi147@slb.com>
2022-10-26 21:10:21 +0800

    feat: add other conditions

--- FizzBuzz/CountGame.cs
+++ FizzBuzz/CountGame.cs
@@ -8,12 +8,32 @@ namespace FizzBuzz
         {
             if (number % 3 == 0)
             {
+                if (number % 5 == 0)
+                {
+                    return "FizzBuzz";
+                }
+
+                if (number % 7 == 0)
+                {
+                    return "FizzWhizz";
+                }
+
                 return "Fizz";
             }
 
             if (number % 5 == 0)
             {
+                if (number % 7 == 0)
+                {
+                    return "BuzzWhizz";
+                }
+
                 return "Buzz";
+            }
+
+            if (number % 7 == 0)
+            {
+                return "Whizz";
             }
 
             return number.ToString();
--- FizzBuzzTest/CountGameTest.cs
+++ FizzBuzzTest/CountGameTest.cs
@@ -32,5 +32,41 @@ namespace FizzBuzzTest
             string ordMappingResult = countGame.OrdMapping(number);
             Assert.Equal(expected: "Buzz", ordMappingResult);
         }
+
+        [Fact]
+        public void Should_return_Whizz_when_given_number_multiple_7()
+        {
+            CountGame countGame = new CountGame();
+            int number = 7;
+            string ordMappingResult = countGame.OrdMapping(number);
+            Assert.Equal(expected: "Whizz", ordMappingResult);
+        }
+
+        [Fact]
+        public void Should_return_FizzBuzz_when_given_number_multiple_3_5()
+        {
+            CountGame countGame = new CountGame();
+            int number = 15;
+            string ordMappingResult = countGame.OrdMapping(number);
+            Assert.Equal(expected: "FizzBuzz", ordMappingResult);
+        }
+
+        [Fact]
+        public void Should_return_FizzWhizz_when_given_number_multiple_3_7()
+        {
+            CountGame countGame = new CountGame();
+            int number = 21;
+            string ordMappingResult = countGame.OrdMapping(number);
+            Assert.Equal(expected: "FizzWhizz", ordMappingResult);
+        }
+
+        [Fact]
+        public void Should_return_BuzzWhizz_when_given_number_multiple_5_7()
+        {
+            CountGame countGame = new CountGame();
+            int number = 35;
+            string ordMappingResult = countGame.OrdMapping(number);
+            Assert.Equal(expected: "BuzzWhizz", ordMappingResult);
+        }
     }
 }
