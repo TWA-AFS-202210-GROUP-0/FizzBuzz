c1adc284c52c16525c5f1ef8bf9a75317187eb8e
binrui <bmu2@slb.com>
2022-10-26 21:45:49 +0800

    test: add tests for the normal number case and the requirement 2.

--- FizzBuzz/CountOff.cs
+++ FizzBuzz/CountOff.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace FizzBuzz
 {
     public class CountOff
     {
-        public string Game(int number)
+        //return number % 7 == 0 ? "FizzBuzzWizz" : "FizzBuzz";
+        public string GetRequirementOne(int number)
         {
             return number.ToString();
+        }
+
+        public string GetRequirementTwo(int number)
+        {
+           if (number > 0)
+            {
+                if (number % 3 == 0) { return "Fizz"; }
+                if (number % 5 == 0) { return "Buzz"; }
+                if (number % 7 == 0) { return "Whizz"; }
+                return number.ToString();
+            }
+            else { return "Invalid negative Number!"; }
+        }
+
+        public string GetRequirementThree(int number)
+        {
+            if (number > 0)
+            {
+                string res = string.Empty;
+                if (number % 3 == 0) { res += "Fizz"; }
+                if (number % 5 == 0) { res += "Buzz"; }
+                if (number % 7 == 0) { res += "Whizz"; }
+                if (res.Length > 0)
+                {
+                    return res;
+                }
+                else
+                {
+                    return number.ToString();
+                }
+            }
+            else { return "Invalid negative Number!";  }
         }
     }
 }
--- FizzBuzzTest/CountOffTest.cs
+++ FizzBuzzTest/CountOffTest.cs
@@ -13,8 +13,41 @@ namespace FizzBuzzTest
             CountOff countNumber = new CountOff();
             int number = 3;
             //then
-            var result = countNumber.Game(number);
+            var result = countNumber.GetRequirementOne(number);
             Assert.Equal("3", result);
+        }
+
+        [Fact]
+        public void Should_return_Fizz_when_CountOff_is_multiples_of_three()
+        {
+            //given
+            CountOff countNumber = new CountOff();
+            int number = 9;
+            //then
+            var result = countNumber.GetRequirementTwo(number);
+            Assert.Equal("Fizz", result);
+        }
+
+        [Fact]
+        public void Should_return_Buzz_when_CountOff_is_multiples_of_five()
+        {
+            //given
+            CountOff countNumber = new CountOff();
+            int number = 10;
+            //then
+            var result = countNumber.GetRequirementTwo(number);
+            Assert.Equal("Buzz", result);
+        }
+
+        [Fact]
+        public void Should_return_Whizz_when_CountOff_is_multiples_of_seven()
+        {
+            //given
+            CountOff countNumber = new CountOff();
+            int number = 14;
+            //then
+            var result = countNumber.GetRequirementTwo(number);
+            Assert.Equal("Whizz", result);
         }
     }
 }
